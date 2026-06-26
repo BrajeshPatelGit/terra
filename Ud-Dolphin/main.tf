83521d3d78ec50dbb7b0262f47dbe721cd1d0fd3
BrajeshPatelGit <brajeshpatel@gmail.com>
2026-06-26 17:09:21 +0530

    updated the aws profile to use the variable "profile" for authentication.

--- Ud-Dolphin/main.tf
+++ Ud-Dolphin/main.tf
@@ -5,3 +5,12 @@ resource "aws_vpc" "prd_vpc" {
     Name = "prd-vpc"
   }
 }
+resource "aws_s3_bucket" "terra_bucket" {
+  bucket = var.s3_bucket_name
+  acl    = "private"
+
+  tags = {
+    Name        = "Terraform State Bucket"
+    Environment = "Dev"
+  }
+}
--- Ud-Dolphin/variables.tf
+++ Ud-Dolphin/variables.tf
@@ -4,8 +4,20 @@ variable "region" {
   default     = "ap-south-1"
 }
 
+variable "profile" {
+  description = "AWS CLI profile to use for authentication"
+  type        = string
+  default     = "default"
+}
+
 variable "vpc_cidr" {
   description = "CIDR block for the VPC"
   type        = string
   default     = "10.0.0.0/16"
+}
+
+variable "s3_bucket_name" {
+  description = "Name of the S3 bucket for Terraform state"
+  type        = string
+  default     = "terraform-state-bucket-1234567890"
 }
