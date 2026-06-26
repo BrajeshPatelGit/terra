3a20045bcb25dcfc82169dc56b9575772de5b0c3
BrajeshPatelGit <brajeshpatel@gmail.com>
2026-06-26 17:47:18 +0530

    Removed the deprecated acl argument from the S3 bucket resource Added an aws_s3_bucket_acl resource for the bucket ACL

--- Ud-Dolphin/main.tf
+++ Ud-Dolphin/main.tf
@@ -7,10 +7,14 @@ resource "aws_vpc" "prd_vpc" {
 }
 resource "aws_s3_bucket" "terra_bucket" {
   bucket = var.s3_bucket_name
-  acl    = "private"
 
   tags = {
     Name        = "Terraform State Bucket"
     Environment = "Dev"
   }
+}
+
+resource "aws_s3_bucket_acl" "terra_bucket_acl" {
+  bucket = aws_s3_bucket.terra_bucket.id
+  acl    = "private"
 }
