9e25ea61858f4a44875d384606c644e8b2a2b2dc
BrajeshPatelGit <brajeshpatel@gmail.com>
2026-06-29 17:21:29 +0530

    added details for EC2

--- Transit-GW/main.tf
+++ Transit-GW/main.tf
@@ -187,11 +187,11 @@ resource "aws_security_group" "public" {
 resource "aws_instance" "this" {
   for_each = var.vpcs
 
-  ami                    = data.aws_ami.amazon_linux_2.id
-  instance_type          = "t2.micro"
-  subnet_id              = aws_subnet.public[each.key].id
-  key_name               = var.instance_key_pairs[each.key]
-  vpc_security_group_ids = [aws_security_group.public[each.key].id]
+  ami                         = data.aws_ami.amazon_linux_2.id
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.public[each.key].id
+  key_name                    = var.instance_key_pairs[each.key]
+  vpc_security_group_ids      = [aws_security_group.public[each.key].id]
   associate_public_ip_address = true
 
   tags = {
