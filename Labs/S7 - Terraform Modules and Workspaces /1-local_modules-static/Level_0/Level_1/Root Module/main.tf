d87989efd936099d131f8f061020468d991f9cde
BrajeshPatelGit <brajeshpatel@gmail.com>
2026-07-07 11:41:50 +0530

    Terraform Module working code

--- Labs/S7 - Terraform Modules and Workspaces /1-local_modules-static/Level_0/Level_1/Root Module/main.tf
+++ Labs/S7 - Terraform Modules and Workspaces /1-local_modules-static/Level_0/Level_1/Root Module/main.tf
@@ -1,13 +1,13 @@
 
 module "ec2module_1" {
-  source       = "../EC2_Module_1" 
+  source = "../EC2_Module_1"
 }
 
 module "ec2module_2" {
-  source       = "./EC2_Module_2"
+  source = "./EC2_Module_2"
 }
 
 module "sgmodule" {
-  source   = "../../Security_Modules/Sec_Grp_Module"
+  source = "../../Security_Modules/Sec_Grp_Module"
 }
 
