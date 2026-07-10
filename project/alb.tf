7eccc365b640414958420bf0309da65091240c97
BrajeshPatelGit <brajeshpatel@gmail.com>
2026-07-10 07:39:36 +0530

    added the required modules and separate the SG file

--- project/alb.tf
+++ project/alb.tf
@@ -1,3 +1,26 @@
+resource "aws_lb" "web_alb" {
+  name               = "WebALB"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [module.alb_sg.security_group_id]
+  subnets            = module.vpc.public_subnets
+
+  tags = {
+    Name = "WebALB"
+  }
+}
+
+resource "aws_lb_listener" "web_listener" {
+  load_balancer_arn = aws_lb.web_alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.web_tg.arn
+  }
+}
+
 resource "aws_lb_target_group" "web_tg" {
   name        = "WebTG"
   port        = 80
